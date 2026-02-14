f0a19479f73f4cd405817fa918f85b079675f8bf
hw <huyvu.hw@gmail.com>
2026-02-14 22:36:12 +0700

    revert to windsuf

--- lua/plugins/suggetion.lua
+++ lua/plugins/suggetion.lua
@@ -1,40 +1,55 @@
 return {
 	{
-		"supermaven-inc/supermaven-nvim",
+		"Exafunction/windsurf.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		event = "BufReadPre",
 		config = function()
-			require("supermaven-nvim").setup({
-				-- Gán các phím tắt của cậu
-				keymaps = {
-					accept_suggestion = "<M-k>", -- Alt + k: Nhận toàn bộ
-					accept_word = "<M-w>", -- Alt + w: Nhận từng từ (Accept Word)
-					clear_suggestion = "<M-c>", -- Alt + c: Xóa gợi ý
-
-					-- BỔ SUNG THEO YÊU CẦU CỦA CẬU:
-					accept_line = "<M-l>", -- Alt + l: Nhận từng dòng (Accept Line)
-					-- Lưu ý: Supermaven bản nvim hiện chưa hỗ trợ phím tắt mặc định cho cycle (đổi gợi ý)
-					-- trong bảng keymaps của setup, nhưng mình sẽ dùng lệnh hoặc phím tắt bên dưới.
+			require("codeium").setup({
+				enable_chat = true,
+				enable_cmp_source = false,
+				virtual_text = {
+					enabled = true,
+					virtual_text_priority = 65535,
+					default_filetype_enabled = true,
+					filetypes = {
+						TelescopePrompt = false,
+						["neo-tree"] = false,
+						["dap-repl"] = false,
+					},
+					map_keys = true,
+					key_bindings = {
+						accept = "<M-k>",
+						accept_word = "<M-w>",
+						accept_line = "<M-l>",
+						clear = "<M-c>",
+						next = "<M-]>",
+						prev = "<M-[>",
+					},
 				},
-
-				color = {
-					suggestion_color = "#808080",
-					cterm = 244,
+				workspace_root = {
+					use_lsp = true,
+					paths = { ".git", "Cargo.toml", "pubspec.yaml", "package.json", "composer.json" },
 				},
-				disable_inline_completion = false,
-				disable_keymaps = false,
 			})
-
-			-- Cấu hình phím tắt bổ sung cho Bật/Tắt và Đổi gợi ý
-			local api = require("supermaven-nvim.api")
-
-			-- Alt + t để Bật/Tắt gợi ý (Toggle)
-			vim.keymap.set("n", "<M-t>", function()
-				api.toggle()
-				vim.notify("Supermaven: " .. (api.is_running() and "Đã bật" or "Đã tắt"))
-			end, { desc = "Supermaven Toggle" })
-
-			-- Đối với việc đổi gợi ý khác (Cycle):
-			-- Hiện tại Supermaven tập trung vào 1 gợi ý duy nhất có độ chính xác cao nhất (fastest),
-			-- Nếu cậu muốn thử gợi ý khác, cậu có thể gõ tiếp để AI tự điều chỉnh theo ngữ cảnh mới.
 		end,
+
+		-- === CẤU HÌNH PHÍM TẮT TẠI ĐÂY ===
+		keys = {
+			-- Phím tắt Bật/Tắt AI (Toggle)
+			{
+				"<leader>at",
+				"<cmd>Codeium Toggle<cr>",
+				desc = "AI: Bật/Tắt Gợi ý (Toggle)",
+			},
+
+			-- Tớ cũng thêm luôn phím mở Chat nhanh (nếu cậu thích dùng trình duyệt)
+			{
+				"<leader>ac",
+				"<cmd>Codeium Chat<cr>",
+				desc = "AI: Chat (Browser)",
+			},
+		},
 	},
 }
