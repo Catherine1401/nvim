8945013ca259db5063bd9d20d2c3b6478209c89b
huyvv <huyvv@vsystem.vn>
2026-08-29 16:27:58 +0700

    refactor(nvim-surround): replace adding bracket from S to Z

--- lua/plugins/nvim_surround.lua
+++ lua/plugins/nvim_surround.lua
@@ -1,8 +1,16 @@
 return {
 	"kylechui/nvim-surround",
-	version = "*",
+	version = "^4.0.0",
 	event = "VeryLazy",
+	init = function()
+		-- Tắt default visual mapping (S và gS) trước khi plugin load
+		vim.g.nvim_surround_no_visual_mappings = true
+	end,
 	config = function()
 		require("nvim-surround").setup({})
+
+		-- Map lại bằng <Plug> mappings
+		vim.keymap.set("x", "Z", "<Plug>(nvim-surround-visual)", { desc = "Surround visual" })
+		vim.keymap.set("x", "gZ", "<Plug>(nvim-surround-visual-line)", { desc = "Surround visual line" })
 	end,
 }
